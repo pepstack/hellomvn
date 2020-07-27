8637a15474ca28bb27d15913edef3ee3317678d4
pepstack.com <clark211@qq.com>
2020-07-27 16:11:51 +0800

    first commit

---
+++ src/main/java/com/github/hellomvn/App.java
@@ -0,0 +1,10 @@
+package com.github.hellomvn;
+
+
+public class App {
+    public static void main( String[] args ) {
+        Dog dog = new Dog("Hello Dog!");
+        
+        System.out.println( dog.getName() );
+    }
+}
